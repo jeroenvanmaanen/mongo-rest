8fccf5f37ff4edcb263ab4e9dc3a6bdd995dddad
Svetlana Linuxenko <linuxenko@yahoo.com>
2015-11-02 17:29:06 +0200

    Initial rest api server

---
+++ index.js
@@ -0,0 +1,48 @@
+/*
+	Availble environment options:
+	ME_CONFIG_DBSTRING = mongodb://user:password@host:port/database
+	ME_CONFIG_READONLY = y  
+	ME_CONFIG_APIKEY = apirequestkey
+	ME_CONFIG_ROOTURL = /api/
+*/
+
+
+var express = require('express');
+var expressMongoRest = require('express-mongo-rest');
+var app = express();
+
+var options = {
+	origin: '*',
+	methods: process.env['ME_CONFIG_READONLY'] ? 'GET,OPTIONS,HEAD' : 'GET,PUT,POST,DELETE,HEAD,OPTIONS',
+	headers: 'Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override',
+	apiKey : process.env['ME_CONFIG_APIKEY'] || null ,
+	db : process.env['ME_CONFIG_DBSTRING'] || 'mongodb://localhost/27017/test',
+	rootURL : process.env['ME_CONFIG_ROOTURL'] || '/api/' 
+};
+
+app.use(function(req,res,next) {
+	if (options.methods.split(',').indexOf(req.method) != -1) {
+		if (options.apiKey && options.apiKey != req.query.apiKey) {
+			res.status(403).send();
+			return;
+		} else {
+			delete req.query.apiKey;
+		}
+		next();
+	} else {
+		res.status(405).send(); // method not allowed
+	}
+});
+
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', options.origin);
+	res.setHeader('Access-Control-Allow-Methods', options.methods);
+	res.setHeader('Access-Control-Allow-Headers', options.headers);
+	next();
+});
+
+app.use(options.rootURL, expressMongoRest(options.db));
+
+var server = app.listen(3000, function () {
+    console.log('Listening on Port', server.address().port);
+})
